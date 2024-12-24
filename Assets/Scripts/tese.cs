641232cf73413990ba7aece631c5ced1b039f8ad
fujisawakoki <kouki8499@gmail.com>
2024-12-25 02:51:50 +0900

    teste

--- Assets/Scripts/tese.cs
+++ Assets/Scripts/tese.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testr222 : MonoBehaviour
+public class tese : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
