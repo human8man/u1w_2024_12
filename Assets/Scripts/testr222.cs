f83c7f50d01b7733b95f262c1b6f57d3087e321e
fujisawakoki <kouki8499@gmail.com>
2024-12-25 02:49:26 +0900

    test1122

--- Assets/Scripts/testr222.cs
+++ Assets/Scripts/testr222.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 2311 : MonoBehaviour
+public class testr222 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
