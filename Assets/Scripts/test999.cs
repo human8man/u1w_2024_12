cc6c6d536e3b409d9bf25f9567eecf0d2f1c04b2
hinayamayarou <162869350+hinayamayarou@users.noreply.github.com>
2024-12-25 02:38:51 +0900

    test

--- Assets/Scripts/test999.cs
+++ Assets/Scripts/test999.cs
@@ -7,7 +7,7 @@ public class test999 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // test : ‚à‚Ý‚¶‚Î’Ç‰Á.
+        // ‚à‚Ý‚¶‚Î’Ç‰Á.
     }
 
     // Update is called once per frame
