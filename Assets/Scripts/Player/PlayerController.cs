cc0b222b10713642511acd9d9e71ebbbb24558b1
hinayamayarou <162869350+hinayamayarou@users.noreply.github.com>
2024-12-27 01:46:20 +0900

    プレイヤーの移動 移動アニメーション（未完成）付き

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // スプライト.
+    public Sprite SpriteIdle;
+    public Sprite SpriteUp;
+    public Sprite SpriteDown;
+    public Sprite SpriteLeft;
+    public Sprite SpriteRight;
+
+    private SpriteRenderer SpriteRenderer;
+
+    public Rigidbody2D Rb;
+    Vector2 BeforeVel;             //前回の移動.
+
+    private const float Speed = 7.0f;   // 移動スピード.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        BeforeVel = Rb.velocity;
     }
 
     // Update is called once per frame
@@ -20,9 +37,46 @@ public class PlayerController : MonoBehaviour
     // キー入力.
     void KeyInput()
     {
-        if (Input.GetButtonDown("Jump"))
-        {
+        // 入力の取得
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
 
+
+        // 移動処理
+        Vector2 moveDirection = new Vector2(horizontalMove, verticalMove);
+        Rb.velocity = moveDirection * Speed;
+
+        // プレイヤーの画像変更.
+        ChangeSprite();
+    }
+
+    void ChangeSprite()
+    {
+        Vector2 nowVel = Rb.velocity;
+
+        if (BeforeVel == nowVel)
+        {
+            SpriteRenderer.sprite = SpriteIdle;     // アイドル状態.
+            return;
         }
+
+        if (BeforeVel.x < nowVel.x)
+        {
+            SpriteRenderer.sprite = SpriteRight;    // 右.
+        }
+        else if (BeforeVel.x > nowVel.x)
+        {
+            SpriteRenderer.sprite = SpriteLeft;     // 左.
+        }
+        else if (BeforeVel.y < nowVel.y)
+        {
+            SpriteRenderer.sprite = SpriteUp;       // 上.
+        }
+        else if (BeforeVel.x > nowVel.x)
+        {
+            SpriteRenderer.sprite = SpriteDown;     // 下.
+        }
+
+        BeforeVel = nowVel;
     }
 }
