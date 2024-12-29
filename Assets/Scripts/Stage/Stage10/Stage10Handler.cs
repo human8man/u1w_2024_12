bbe1d4f695cdb58c1707090e526c327f0e8a06cf
ogata <malineog.1268@gmail.com>
2024-12-29 13:30:39 +0900

    ステージ10UIなし仮置き

---
+++ Assets/Scripts/Stage/Stage10/Stage10Handler.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Stage
+{
+    public class Stage10Handler : MonoBehaviour
+    {
+        void Update()
+        {
+            if (WordController.Instance.inactiveWord == "気体")
+            {
+                
+            }
+           
+        }
+    }
+}
