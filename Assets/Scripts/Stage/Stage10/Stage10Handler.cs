7eea23cf9501b5d1c57f7f7ef86b798f5f453a0b
ogata <malineog.1268@gmail.com>
2024-12-29 16:03:14 +0900

    泡ゲージ追加

--- Assets/Scripts/Stage/Stage10/Stage10Handler.cs
+++ Assets/Scripts/Stage/Stage10/Stage10Handler.cs
@@ -1,17 +1,40 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Stage
 {
     public class Stage10Handler : MonoBehaviour
     {
+        [SerializeField] Stage10PlayerAirUI _airUI;
+        [SerializeField] float _maxAirTime;
+        [SerializeField] GameObject _playerObject;
+        float _nowTime = 0;
+
+        void Start()
+        {
+            WordController.Instance.AddWord("水");
+            WordController.Instance.AddWord("液体");
+        }
+
         void Update()
         {
+            _playerObject.SetActive(WordController.Instance.inactiveWord != "固体");
             if (WordController.Instance.inactiveWord == "気体")
             {
-                
+                _airUI.gameObject.SetActive(true);
+                _nowTime += Time.deltaTime;
+                if (_nowTime > _maxAirTime)
+                {
+                    Debug.Log("死んだ");
+                }
+                _airUI.UpdateUI(1 - _nowTime / _maxAirTime);
             }
-           
+            else
+            {
+                _airUI.gameObject.SetActive(false);
+                _nowTime = 0;
+            }
         }
     }
 }
--- Assets/Scripts/Stage/Stage10/Stage10PlayerAirUI.cs
+++ Assets/Scripts/Stage/Stage10/Stage10PlayerAirUI.cs
@@ -1,28 +1,33 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Stage
 {
     public class Stage10PlayerAirUI : MonoBehaviour
     {
-        [SerializeField] float _maxTime;
-        float _nowTime = 0;
-        public Action OnAirZero;
+        [SerializeField] SpriteRenderer _renderer;
+        [SerializeField] Sprite[] _sprites;
+        [SerializeField] Transform _playerTransform;
+        [SerializeField] Vector2 _offset;
+        public void UpdateUI(float rate)
+        {
+            if (rate < 0)
+            {
+                _renderer.sprite = _sprites[0];
+            }
+            else
+            {
+                float oneRate = 1 / (float)(_sprites.Length - 1);
+                int index = Mathf.CeilToInt(rate / oneRate);
+                _renderer.sprite = _sprites[index];   
+            }
+        }
 
         void Update()
         {
-            _nowTime += Time.deltaTime;
-            if (_nowTime >= _maxTime)
-            {
-                OnAirZero?.Invoke();
-                gameObject.SetActive(false);
-            }
-            UpdateUI(1 - _nowTime / _maxTime);
-        }
-
-        void UpdateUI(float rate)
-        {
-            
+            Vector2 newPosition = (Vector2)_playerTransform.position + _offset;
+            transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
         }
     }
 }
